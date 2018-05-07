517257f51a7f95d3ebe626143014176790a23cc5
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-05-08 00:09:14 +0300

    Make python3.6 default by symbolic links

--- setup.sh
+++ setup.sh
@@ -1,30 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
+ 
+echo ======= update source list for packages and versions that can be installed
+sudo apt-get update
 
-#update the image
-echo "Updating the system..."
+echo ======= add python3.6 PPA == Personal Packages Archives ==  to the list of sources
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+
+echo ======= update python 3.6 among the list of packages that can be installed
 sudo apt-get update
-sudo apt-get -y dist-upgrade
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:jonathonf/python-3.6
-sudo apt-get update
-sudo apt-get install -y python3.6 python3-pip python3-dev nginx gunicorn
-sudo apt-get install -y --no-install-recommends apt-utils python3-apt
+
+echo ======= install python3.6
+sudo apt-get install -y python3.6
+
+echo ======= make python3.6 default the python3
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 10
-sudo update-alternatives --config python3
+sudo update-alternatives --config -y python3
 
-# cloning the project
-sudo mkdir /home/ubuntu/trial/ && cd /home/ubuntu/trial/
-echo "cloning the repository from github..."
-sudo git clone https://github.com/philophilo/yummy_api.git
+echo ======= install gunicorn, pip and nginx
+sudo apt-get install -y python3-pip nginx gunicorn
 
-# create the virtual environment
-echo "creating varitual environment.."
-sudo pip install virtualenv
-sudo virtualenv venv2
-source venv2/bin/activate
-cd yummy_api
+echo ======= install virtualenv
+sudo pip3 install virtualenv
 
-# install project requirments
-echo "install requirments..."
-pip install -r requirements.txt
+echo ======= creating the virtual environment
+sudo virtualenv -p python3 venv
+
+echo ======= activate the virtual env
+source venv/bin/activate
+
+echo ======= Clone the repo
+sudo git clone https://github.com/philophilo/yummy_api_script.git
+
+echo ======= Entering the project folder
+cd yummy_api_script
+
+echo ======= installing all the project requirements
+sudo pip3 install -r requirements.txt
