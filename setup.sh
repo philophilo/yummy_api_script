8c430ecf5aec30d0397488eff587a845ba31f23b
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-05-08 00:24:12 +0300

    Add step out of repo

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+echo ======= step out of git repo
+cd ..
  
 echo ======= update source list for packages and versions that can be installed
 sudo apt-get update
@@ -12,7 +15,7 @@ sudo apt-get update
 echo ======= install python3.6
 sudo apt-get install -y python3.6
 
-echo ======= make python3.6 default the python3
+echo ======= make python3.6 default the python3 by symbolic links in /etc/alternatives
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 10
 sudo update-alternatives --config -y python3
