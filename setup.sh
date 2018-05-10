09af1b9321911dda73d0e1d740122797e6406c3e
Philip Kwesiga <phil.kwesiga@gmail.com>
2018-05-10 14:14:53 +0300

    Update python configurations

--- setup.sh
+++ setup.sh
@@ -31,6 +31,9 @@ install_python_dependencies(){
     sudo apt-get install -y build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libssl-dev
     sudo easy_install greenlet
     sudo easy_install gevent
+    sudo apt-get install python3-gdbm
+    export LC_ALL=en_US.UTF-8 
+    export LANG=en_US.UTF-8
 }
 
 create_virtual_environment(){
